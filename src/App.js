656b59ea0983ea29b2ec382de7185fa6de5619f6
solace1987 <oluwashola.ogundipe@gmail.com>
2019-08-13 08:26:54 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+// import{Record} from "./components/recordForm"
+//import{Heading} from "./components/Header"
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+//import {Divider} from 'semantic-ui-react';
+//import{IncidentTable}from "./components/incidentTable"
+
+/*<Heading/>
+  <Record/>
+  <Divider />
+  <IncidentTable/>*/
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div style={{padding:40}}>
+  <h2>HELLO WORLD</h2>
+</div>
+
+    
   );
 }
 
