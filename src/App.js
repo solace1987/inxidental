b9103eef708c2311160381bfde6064c00fb915fa
solace1987 <oluwashola.ogundipe@gmail.com>
2019-08-13 08:30:34 +0100

    incident form commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
-// import{Record} from "./components/recordForm"
-//import{Heading} from "./components/Header"
+import{Record} from "./components/recordForm"
+import{Heading} from "./components/Header"
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
-//import {Divider} from 'semantic-ui-react';
-//import{IncidentTable}from "./components/incidentTable"
-
-/*<Heading/>
-  <Record/>
-  <Divider />
-  <IncidentTable/>*/
+import {Divider} from 'semantic-ui-react';
+import{IncidentTable}from "./components/incidentTable"
 
 function App() {
   return (
 <div style={{padding:40}}>
-  <h2>HELLO WORLD</h2>
+  <Heading/>
+  <Record/>
+  <Divider />
+  <IncidentTable/>
 </div>
 
     
