f0799e7d6d67ebbf8cb0d74395d007edd219ba79
solace1987 <oluwashola.ogundipe@gmail.com>
2019-08-18 06:43:24 +0100

    update

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import{Heading} from "./components/Header"
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 import {Divider} from 'semantic-ui-react';
-import{IncidentTable}from "./components/incidentTable"
-
+//import{IncidentTable}from "./components/incidentTable"
+//
 function App() {
   return (
 <div style={{padding:40}}>
   <Heading/>
   <Record/>
   <Divider />
-  <IncidentTable/>
+  
 </div>
 
     
--- src/components/recordForm.js
+++ src/components/recordForm.js
@@ -6,51 +6,70 @@ export class Record extends Component{
     constructor(props){
     super(props)
 
-        this.state={date:'',user:'',department:'',incident:'',category:'',description:'',priority:'', resolution:'',status:'',incidentInfo:{}}
+        this.state={data:{},user:'',dept:'',
+                    incident:'',category:'',descr:'',
+                    priority:'', resolu:'',status:'',
+                   }
+
+      this.handleChange=this.handleChange.bind(this);
+      this.handleSubmit=this.handleSubmit.bind(this);
+    }
+
+
+      handleChange(e) {
+    this.setState({[e.target.name]:e.target.value});
+  }
+    handleSubmit(e){
+      /*let data=this.state;
+      this.setState({data:data})  */      
+      console.log(this.state)
+      
     }
 
     render(){
     return(
-  <Form>
+  <Form onSubmit={this.handleSubmit}>
     <Grid stackable columns={2}>
       <Grid.Column>
         <Form.Field>
           <label>User</label>
-          <input placeholder='First Name'  />
+          <input name='user' onChange={this.handleChange} type='text' placeholder='Name' className='info' value={this.state.user} />
         </Form.Field>
         <Form.Field>
           <label>Incident</label>
-          <input placeholder='Last Name' />
+          <input onChange={this.handleChange} name='incident' placeholder='Incident' value={this.state.incident}/>
         </Form.Field>
         <Form.Field>
           <label>Department</label>
-           <Select placeholder='Department' options={[{value:'hr',text:"HR"},{value:'finance',text:"Finance"},{value:'purchasing',text:"Purchasing"},{value:'Prepress',text:"Prepress"}]} />
+           <Select  onChange={(e,data) => {this.setState({dept:data.value})}} value={this.state.dept} name='dept' placeholder='Department' options={[{value:'HR',text:"HR"},{value:'finance',text:"Finance"},{value:'purchasing',text:"Purchasing"},{value:'Prepress',text:"Prepress"}]} />
         </Form.Field>
         <Form.Field>
           <label>Priority</label>
-           <Select placeholder='Department' options={[{value:'Critical',text:"Critical"},{value:'high',text:"High"},{value:'low',text:"Low"}]} />
+           <Select onChange={(e,data) => {this.setState({priority:data.value})}} value={this.state.priority}
+                    name='priority' placeholder='Department' 
+                    options={[{value:'Critical',text:"Critical"},{value:'high',text:"High"},{value:'low',text:"Low"}]} />
         </Form.Field>
       </Grid.Column>
       
     <Grid.Column>
       <Form.Field>
       <label>Description</label>
-        <TextArea placeholder='Description' style={{ minHeight:40 }} />
+        <TextArea onChange={this.handleChange} value={this.state.descr} name='descr' placeholder='Description' style={{ minHeight:40 }} />
      </Form.Field>
      <Form.Field>
      <label>Resolution</label>
-        <TextArea placeholder='Resolution' style={{ minHeight: 40 }} />
+        <TextArea onChange={this.handleChange} value={this.state.resolu} name='resolu' placeholder='Resolution' style={{ minHeight: 40 }} />
      </Form.Field>
      <Form.Field>
           <label>Status</label>
-           <Select placeholder='Status' options={[{value:'open',text:"Open"},{value:'closed',text:"Closed"}]} />
+           <Select onChange={(e,data) => {this.setState({status:data.value})}} value={this.state.status} name='status' placeholder='Status' options={[{value:'open',text:"Open"},{value:'closed',text:"Closed"}]} />
         </Form.Field>
          
   </Grid.Column>
   </Grid>
 
   <Grid style={{padding:15}} centered='true'>
-      <Button size='big' color='red'  type='submit'>Submit</Button>
+      <Button size='big' color='red'  type='submit' >Submit</Button>
       </Grid>
 </Form>
 
